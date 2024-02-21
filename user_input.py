fa4ca2cba8959830d2263d08291c5a553b45e8c1
phalamat <159577130+phalamat@users.noreply.github.com>
2024-02-21 06:56:54 +0200

    Python Assignment 1

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# This program will ask the user for their name, age, and location and then prints out a personalized message.
+# Developer : Phala Mathobela
+
+name = input("Please capture your name: ")
+age = input("Please capture your age(in numbers format): ")
+location = input("Please capture your location: ")
+
+print(f"\nHello {name}, you are {age} year(s) old and live in {location}.")
+
+# End of the program
